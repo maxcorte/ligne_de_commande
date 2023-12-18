deb8d749fade789fbbcdb8f0ce2565444724c578
maxcorte <m.delcorte@students.ephec.be>
2023-12-18 21:26:44 +0100

    try except raise + linter
    ajout de raise et du linter

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tkinter import filedialog
 
 
 def trier_par_extension(repertoire_source, repertoire_destination):
-    try:
+    if repertoire_source and repertoire_destination:
         # Assurez-vous que les répertoires de destination existent
         if not os.path.exists(repertoire_destination):
             os.makedirs(repertoire_destination)
@@ -30,108 +30,85 @@ def trier_par_extension(repertoire_source, repertoire_destination):
 
         print("Tri des fichiers par extension terminé.")
 
-    except Exception as e:
-        print(f"Erreur lors du tri des fichiers par extension : {e}")
-
 
 def choisir_fichier():
-    try:
-        root = tk.Tk()
-        root.withdraw()  # Ne pas afficher la fenêtre principale
+    root = tk.Tk()
+    root.withdraw()  # Ne pas afficher la fenêtre principale
 
-        fichier_selectionne = filedialog.askopenfilename(title="Sélectionnez un fichier")
+    fichier_selectionne = filedialog.askopenfilename(title="Sélectionnez un fichier")
+    if fichier_selectionne:
         return fichier_selectionne
-
-    except Exception as e:
-        print(f"Erreur lors de la sélection du fichier : {e}")
-        return None
+    else:
+        raise FileNotFoundError("Aucun fichier sélectionné.")
 
 
 def creer_fichier(dossier, fichier):
     chemin_fichier = os.path.join(dossier, fichier)
-    try:
-        if chemin_fichier:
-            with open(chemin_fichier, 'w'):
+    if chemin_fichier:
+        with open(chemin_fichier, 'w') as file:
+            if file:
                 print(f"Fichier '{chemin_fichier}' créé avec succès.")
-
-    except PermissionError as e:
-        print(f"Erreur de permission : {e}")
-    except Exception as e:
-        print(f"Erreur lors de la création du fichier : {e}")
+            else:
+                raise PermissionError(f"Impossible de créer le fichier '{chemin_fichier}'.")
 
 
 def supprimer_fichier(supp):
-    try:
-        if supp:
-            os.remove(supp)
-            print(f"Fichier '{supp}' supprimé avec succès.")
-
-    except FileNotFoundError:
-        print(f"Le fichier '{supp}' n'existe pas.")
-    except Exception as e:
-        print(f"Erreur lors de la suppression du fichier : {e}")
+    if supp:
+        os.remove(supp)
+        print(f"Fichier '{supp}' supprimé avec succès.")
 
 
 def renommer_fichier(ancien_nom):
     if ancien_nom:
         nouveau_nom = input("Entrez le nouveau nom du fichier : ")
-        if nouveau_nom:
-            try:
-                # Utilisez os.rename pour renommer le fichier dans le même dossier
-                os.rename(ancien_nom, os.path.join(os.path.dirname(ancien_nom), nouveau_nom))
+        if nouveau_nom and nouveau_nom != ancien_nom:
+            # Utilisez os.rename pour renommer le fichier dans le même dossier
+            if os.path.exists(os.path.join(os.path.dirname(ancien_nom), nouveau_nom)):
                 print(f"Fichier renommé de '{ancien_nom}' à '{nouveau_nom}'.")
-            except FileNotFoundError:
-                print(f"Le fichier '{ancien_nom}' n'existe pas.")
-            except OSError as e:
-                print(f"Erreur lors du renommage : {e}")
+            else:
+                raise OSError(f"Le fichier '{ancien_nom}' n'existe pas.")
 
 
 def choisir_dossier():
-    try:
-        root = tk.Tk()
-        root.withdraw()  # Ne pas afficher la fenêtre principale
+    root = tk.Tk()
+    root.withdraw()  # Ne pas afficher la fenêtre principale
 
-        dossier_selectionne = filedialog.askdirectory(title="Sélectionnez un répertoire")
+    dossier_selectionne = filedialog.askdirectory(title="Sélectionnez un répertoire")
+    if dossier_selectionne:
         return dossier_selectionne
-
-    except Exception as e:
-        print(f"Erreur lors de la sélection du dossier : {e}")
-        return None
+    else:
+        raise FileNotFoundError("Aucun répertoire sélectionné.")
 
 
 def interface_utilisateur():
-    try:
-        while True:
-            print("\nMenu:")
-            print("1. Trier des fichiers par extension")
-            print("2. Créer un fichier")
-            print("3. Supprimer un fichier")
-            print("4. Renommer un fichier")
-            print("5. Quitter")
+    while True:
+        print("\nMenu:")
+        print("1. Trier des fichiers par extension")
+        print("2. Créer un fichier")
+        print("3. Supprimer un fichier")
+        print("4. Renommer un fichier")
+        print("5. Quitter")
 
-            choix = input("Choisissez une option (1/2/3/4/5): ")
+        choix = input("Choisissez une option (1/2/3/4/5): ")
 
-            if choix == "1":
-                source = choisir_dossier()
-                destination = choisir_dossier()
-                trier_par_extension(source, destination)
-            elif choix == "2":
-                dossier = choisir_dossier()
-                fichier = input("Entrez le nom du fichier à créer : ")
-                creer_fichier(dossier, fichier)
-            elif choix == "3":
-                supp = choisir_fichier()
-                supprimer_fichier(supp)
-            elif choix == "4":
-                reno = choisir_fichier()
-                renommer_fichier(reno)
-            elif choix == "5":
-                break
-            else:
-                print("Option invalide. Veuillez choisir une option valide.")
-
-    except Exception as e:
-        print(f"Une erreur s'est produite : {e}")
+        if choix == "1":
+            source = choisir_dossier()
+            destination = choisir_dossier()
+            trier_par_extension(source, destination)
+        elif choix == "2":
+            dossier = choisir_dossier()
+            fichier = input("Entrez le nom du fichier à créer : ")
+            creer_fichier(dossier, fichier)
+        elif choix == "3":
+            supp = choisir_fichier()
+            supprimer_fichier(supp)
+        elif choix == "4":
+            reno = choisir_fichier()
+            renommer_fichier(reno)
+        elif choix == "5":
+            break
+        else:
+            print("Option invalide. Veuillez choisir une option valide.")
 
 
 def main():
@@ -139,4 +116,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        interface_utilisateur()
+    except FileNotFoundError as e:
+        print(f"il manque un fichier {e}.")
+    except PermissionError as e:
+        print(f"Erreur de permission : {e}")
+    except OSError as e:
+        print(f"Erreur lors du renommage : {e}")
